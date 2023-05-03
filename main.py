4bd11c166c902db487ef90cba686ed6476f8da69
Aytage <125976794+Aytage@users.noreply.github.com>
2023-05-03 18:11:27 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,32 @@ import pandas as pd
 from datetime import date,datetime
 import os, sys
 import selenium
-
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+
+chrome_options = Options()
+options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+]
+for option in options:
+    chrome_options.add_argument(option)
+
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+#from selenium import webdriver
 # from webdriver_manager.chrome import ChromeDriverManager
 # service = ChromeService(executable_path=ChromeDriverManager().install())
 
-driver = webdriver.Chrome(executable_path=r'C:\Users\Aytac Agayev\Downloads\chromedriver_win32\chromedriver.exe')
+#driver = webdriver.Chrome(executable_path=r'C:\Users\Aytac Agayev\Downloads\chromedriver_win32\chromedriver.exe')
 driver.get('https://www.wsj.com/wsjsitemaps/wsj_google_news.xml')
 driver
 
